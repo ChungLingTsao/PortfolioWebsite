aef9b156e8db8e7437f9f520dec3b7aa66674268
ChungLingTsao <39504754+ChungLingTsao@users.noreply.github.com>
2020-12-04 10:14:53 +1300

    Add files for uploading submitted website project

---
+++ js/anchorNav.js
@@ -0,0 +1,19 @@
+// Anchor Navigation Scrolling Function
+// From https://stackoverflow.com/a/39734042
+jQuery(function($) {
+    $('a[href*=#]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+       && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                .animate({ scrollTop: targetOffset }, 1000); //set scroll speed
+                return false;
+            }
+        }
+    });
+    window.scrollReveal = new scrollReveal(); 
+});
